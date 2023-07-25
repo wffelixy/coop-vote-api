b862917be14cf4c99a69aff231e0859ecb89d19c
Wellington Felix <wffelixy@gmail.com>
2023-07-25 11:10:11 -0300

    Criando o projeto

---
+++ coop-vote-api/src/main/java/com/wfsat/demo/CoopVoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.wfsat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopVoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopVoteApiApplication.class, args);
+	}
+
+}
---
+++ coop-vote-api/src/test/java/com/wfsat/demo/CoopVoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfsat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopVoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
